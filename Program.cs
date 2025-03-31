040892284bce0612cc98f255a0c89f170532fc67
Davi de Castro Oliveira <davicastrooliveira@outlook.com>
2025-03-31 09:56:46 -0300

    Programa que calcula o custo estimado de um ar condicionado

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿double consumo;
+double horas;
+double custoenergia;
+decimal custoestimado;
+
+Console.Write("Digite o consumo (kWh/mês): ");
+consumo = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("\nDigite as horas de uso diárias: ");
+horas = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("\nDigite o custo de energia do aparelho (em R$/kWh): ");
+custoenergia = Convert.ToDouble(Console.ReadLine()!);
+Console.Clear();
+
+Console.WriteLine("--- Custo de Energia ---");
+Console.WriteLine($"\nConsumo do aparelho (em kWh/mês)...: {consumo}");
+Console.WriteLine($"Horas de uso por dia...............: {horas}");
+Console.WriteLine($"Custo da energia (em R$/kWh).......: {custoenergia}");
+
+custoestimado = Convert.ToDecimal(consumo * horas * custoenergia);
+Console.WriteLine($"\nCusto estimado: {custoestimado:C2}");
