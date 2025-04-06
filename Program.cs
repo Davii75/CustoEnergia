20d9dd1d95abf6a5893fb42bf8a6fc57e54ebb52
Davi Oliveira <davicastrooliveira@outlook.com>
2025-04-06 19:31:27 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿double consumo;
+double consumo;
 double horas;
-double custoenergia;
+decimal custoenergia;
 decimal custoestimado;
 
 Console.Write("Digite o consumo (kWh/mês): ");
@@ -8,7 +8,7 @@ consumo = Convert.ToDouble(Console.ReadLine()!);
 Console.Write("\nDigite as horas de uso diárias: ");
 horas = Convert.ToDouble(Console.ReadLine()!);
 Console.Write("\nDigite o custo de energia do aparelho (em R$/kWh): ");
-custoenergia = Convert.ToDouble(Console.ReadLine()!);
+custoenergia = Convert.ToDecimal(Console.ReadLine()!);
 Console.Clear();
 
 Console.WriteLine("--- Custo de Energia ---");
@@ -16,5 +16,5 @@ Console.WriteLine($"\nConsumo do aparelho (em kWh/mês)...: {consumo}");
 Console.WriteLine($"Horas de uso por dia...............: {horas}");
 Console.WriteLine($"Custo da energia (em R$/kWh).......: {custoenergia}");
 
-custoestimado = Convert.ToDecimal(consumo * horas * custoenergia);
+custoestimado = Convert.ToDecimal(consumo * horas) * custoenergia;
 Console.WriteLine($"\nCusto estimado: {custoestimado:C2}");
